2b516ca9186a9a7d4443205874e6766c8e339d31
Ruby-chan 3 <emrex1210@gmail.com>
2025-08-24 14:21:59 +0200

    I AM YOUR GOD NOW IDIOT
    Tools are now downloading, Extract/Repack .DAT Archives is working too btw.

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -33,10 +33,25 @@
             label1 = new Label();
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
+            panel1 = new Panel();
+            label4 = new Label();
+            button4 = new Button();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            button3 = new Button();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            button1 = new Button();
+            label2 = new Label();
             tabPage2 = new TabPage();
             tabPage3 = new TabPage();
+            label5 = new Label();
+            button5 = new Button();
+            progressBar1 = new ProgressBar();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // pictureBox1
@@ -73,6 +88,7 @@
             // 
             // tabPage1
             // 
+            tabPage1.Controls.Add(panel1);
             tabPage1.Location = new Point(4, 24);
             tabPage1.Name = "tabPage1";
             tabPage1.Padding = new Padding(3);
@@ -80,6 +96,105 @@
             tabPage1.TabIndex = 0;
             tabPage1.Text = "Death end re;Quest 1 + 2";
             tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(label2);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(200, 292);
+            panel1.TabIndex = 0;
+            // 
+            // label4
+            // 
+            label4.Location = new Point(5, 134);
+            label4.Name = "label4";
+            label4.Size = new Size(190, 23);
+            label4.TabIndex = 7;
+            label4.Text = "Extracted Folder";
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(162, 160);
+            button4.Name = "button4";
+            button4.Size = new Size(33, 23);
+            button4.TabIndex = 5;
+            button4.Text = "...";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // label3
+            // 
+            label3.Location = new Point(5, 57);
+            label3.Name = "label3";
+            label3.Size = new Size(190, 23);
+            label3.TabIndex = 4;
+            label3.Text = "Selected Archive";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(5, 160);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(151, 23);
+            textBox2.TabIndex = 6;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(162, 83);
+            button3.Name = "button3";
+            button3.Size = new Size(33, 23);
+            button3.TabIndex = 1;
+            button3.Text = "...";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(5, 83);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(151, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(5, 262);
+            button2.Name = "button2";
+            button2.Size = new Size(190, 23);
+            button2.TabIndex = 2;
+            button2.Text = "Repack";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(5, 233);
+            button1.Name = "button1";
+            button1.Size = new Size(190, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Extract";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.BorderStyle = BorderStyle.FixedSingle;
+            label2.Location = new Point(-2, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(200, 37);
+            label2.TabIndex = 0;
+            label2.Text = ".DAT Archives\r\nExtract/Repack\r\n";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // tabPage2
             // 
@@ -101,11 +216,41 @@
             tabPage3.Text = "Mary Skelter 2 + Finale";
             tabPage3.UseVisualStyleBackColor = true;
             // 
+            // label5
+            // 
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label5.Location = new Point(643, 12);
+            label5.Name = "label5";
+            label5.Size = new Size(141, 23);
+            label5.TabIndex = 3;
+            label5.Text = "Toolset";
+            label5.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(643, 38);
+            button5.Name = "button5";
+            button5.Size = new Size(141, 28);
+            button5.TabIndex = 4;
+            button5.Text = "Download";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(643, 72);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(141, 23);
+            progressBar1.TabIndex = 5;
+            // 
             // FormMain
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(progressBar1);
+            Controls.Add(button5);
+            Controls.Add(label5);
             Controls.Add(tabControl1);
             Controls.Add(label1);
             Controls.Add(pictureBox1);
@@ -117,6 +262,9 @@
             Load += FormMain_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -128,5 +276,18 @@
         private TabPage tabPage1;
         private TabPage tabPage2;
         private TabPage tabPage3;
+        private Panel panel1;
+        private Label label2;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Label label4;
+        private Button button4;
+        private Label label3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label5;
+        private Button button5;
+        private ProgressBar progressBar1;
     }
 }
--- FormMain.cs
+++ FormMain.cs
@@ -1,5 +1,7 @@
 using EnigmaToolkit.Properties;
+using System.Diagnostics;
 using System.Drawing.Text;
+using System.Net;
 using System.Reflection;
 
 namespace EnigmaToolkit
@@ -10,11 +12,136 @@ namespace EnigmaToolkit
         {
             InitializeComponent();
             label1.Text = $"Enigma Toolkit\nv{Application.ProductVersion}";
+            progressBar1.Visible = false;
         }
 
         private void FormMain_Load(object sender, EventArgs e)
         {
-            
+            if (!Directory.Exists("Tools"))
+            {
+                Directory.CreateDirectory("Tools");
+            }
+
+            if (File.Exists($@"{AppContext.BaseDirectory}\\Tools\\dds2png.exe") && 
+                File.Exists($@"{AppContext.BaseDirectory}\\Tools\\dds2tex.exe") && 
+                File.Exists($@"{AppContext.BaseDirectory}\\Tools\\dds2tid.exe") && 
+                File.Exists($@"{AppContext.BaseDirectory}\\Tools\\extract_cl3.exe") && 
+                File.Exists($@"{AppContext.BaseDirectory}\\Tools\\extract_dat.exe") && 
+                File.Exists($@"{AppContext.BaseDirectory}\\Tools\\modify_text.exe") &&
+                File.Exists($@"{AppContext.BaseDirectory}\\Tools\\pickup_text.exe") &&
+                File.Exists($@"{AppContext.BaseDirectory}\\Tools\\png2tex.exe") &&
+                File.Exists($@"{AppContext.BaseDirectory}\\Tools\\replace_cl3.exe") &&
+                File.Exists($@"{AppContext.BaseDirectory}\\Tools\\replace_dat.exe") &&
+                File.Exists($@"{AppContext.BaseDirectory}\\Tools\\tex2dds.exe")
+                )
+            {
+                button5.Enabled = false;
+                button5.Text = "Installed";
+            }
+            else
+            {
+                button5.Enabled = true;
+                button5.Text = "Install Tools";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog SelectDATArchive = new OpenFileDialog
+            {
+                Title = "Select DAT Archive",
+                Filter = "DAT Archive (GxArchivedFile*.dat)|GxArchivedFile*.dat",
+                RestoreDirectory = true
+            };
+
+            if (SelectDATArchive.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = SelectDATArchive.FileName;
+                textBox2.Text = SelectDATArchive.FileName + "_extracted";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog SelectExtractedDATArchiveFolder = new FolderBrowserDialog
+            {
+                Description = "Select Extracted DAT Archive Folder",
+                ShowNewFolderButton = true
+            };
+
+            if (SelectExtractedDATArchiveFolder.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = SelectExtractedDATArchiveFolder.SelectedPath;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Extract Button
+            Process ExtractDATArchive = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = $@"{AppContext.BaseDirectory}\\Tools\\extract_dat.exe",
+                    Arguments = textBox1.Text + " -o " + textBox2.Text,
+                    WorkingDirectory = @$"{AppContext.BaseDirectory}\\Tools",
+                }
+            };
+            ExtractDATArchive.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Repack Button
+            Process RepackDATArchive = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = $@"{AppContext.BaseDirectory}\\Tools\\replace_dat.exe",
+                    Arguments = textBox1.Text + " " + textBox2.Text + " -o " + textBox1.Text + "_NEW",
+                    WorkingDirectory = @$"{AppContext.BaseDirectory}\\Tools",
+                }
+            };
+            RepackDATArchive.Start();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            progressBar1.Visible = true;
+            button5.Enabled = false;
+            button5.Text = "Downloading...";
+
+            using (WebClient wc_locale = new WebClient())
+            {
+                wc_locale.DownloadProgressChanged += wc_locale_DownloadProgressChanged;
+                wc_locale.DownloadFileAsync(
+                    // Param1 = Link of file
+                    new System.Uri("https://cdn.discordapp.com/attachments/844635048745369611/1409140663995596890/ExternalToolset.zip?ex=68ac4c02&is=68aafa82&hm=cfeda2977916cc479610bd8f9feb24bae99a7e3568c6f755d9233c5da4748fa3&"),
+                    // Param2 = Path to save
+                    AppContext.BaseDirectory + "ExternalToolset.zip"
+                );
+                wc_locale.DownloadFileCompleted += (s, ev) =>
+                {
+                    if (ev.Error != null)
+                    {
+                        MessageBox.Show("Error downloading file: " + ev.Error.Message);
+                    }
+                    else
+                    {
+                        // Optionally, you can extract the downloaded zip file here
+                        System.IO.Compression.ZipFile.ExtractToDirectory(AppContext.BaseDirectory + "ExternalToolset.zip", AppContext.BaseDirectory + "Tools");
+                        System.IO.File.Delete(AppContext.BaseDirectory + "ExternalToolset.zip"); // Delete the zip file after extraction
+                        button5.Enabled = false;
+                        button5.Text = "Installed";
+                        progressBar1.Visible = false;
+                    }
+                };
+            }
+        }
+
+        void wc_locale_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
         }
     }
 }
