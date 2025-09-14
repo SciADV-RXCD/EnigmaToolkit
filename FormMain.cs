31a7cceb836de4b15454510ad1a46909d9d6c3f1
Ruby-chan 3 <emrex1210@gmail.com>
2025-09-14 18:02:48 +0200

    fix a mistake i made when converting scripts

--- FormMain.cs
+++ FormMain.cs
@@ -488,7 +488,7 @@ namespace EnigmaToolkit
                 StartInfo = new ProcessStartInfo
                 {
                     FileName = $@"{AppContext.BaseDirectory}\\Tools\\modify_text.exe",
-                    Arguments = "--utf8 " + textBox8.Text + " " + textBox7.Text + " -o " + $"{textBox8.Text}_NEW",
+                    Arguments = "--utf8 -N " + textBox8.Text + " " + textBox7.Text + " -o " + $"{textBox8.Text}_NEW",
                     WorkingDirectory = @$"{AppContext.BaseDirectory}\\Tools",
                 }
             };
@@ -549,7 +549,7 @@ namespace EnigmaToolkit
                 StartInfo = new ProcessStartInfo
                 {
                     FileName = $@"{AppContext.BaseDirectory}\\Tools\\modify_text.exe",
-                    Arguments = "--utf8 " + textBox10.Text + " " + textBox9.Text + " -o " + $"{textBox10.Text}_NEW",
+                    Arguments = "--utf8 -N " + textBox10.Text + " " + textBox9.Text + " -o " + $"{textBox10.Text}_NEW",
                     WorkingDirectory = @$"{AppContext.BaseDirectory}\\Tools",
                 }
             };
@@ -676,7 +676,7 @@ namespace EnigmaToolkit
                 StartInfo = new ProcessStartInfo
                 {
                     FileName = $@"{AppContext.BaseDirectory}\\Tools\\modify_text.exe",
-                    Arguments = "--utf8 " + textBox14.Text + " " + textBox13.Text + " -o " + $"{textBox14.Text}_NEW",
+                    Arguments = "--utf8 -N " + textBox14.Text + " " + textBox13.Text + " -o " + $"{textBox14.Text}_NEW",
                     WorkingDirectory = @$"{AppContext.BaseDirectory}\\Tools",
                 }
             };
